306a59f59654ce1812d96982d1cdb625c01dc895
jaindivit1998 <38127185+jaindivit1998@users.noreply.github.com>
2023-01-29 14:36:51 +0530

    Add files via upload

---
+++ DSA_Bracket_balancer/src/bracket/balancer/BalancedBracketsCheckertest.java
@@ -0,0 +1,28 @@
+package bracket.balancer;
+
+public class BalancedBracketsCheckertest {
+	
+	public static void main(String[] args) {
+		
+		test("{[()]}");
+		test("{}");
+		test("[]");
+		test("[{}]");
+		test("[<>]");
+		test("[<<]");
+		test("({[<>]})");
+		test("[]");
+		test("[}");
+		test("[<>");
+		
+	}
+
+	private static void test(String brackets) {
+			
+		BalancedBracketsChecker checker = new BalancedBracketsChecker(brackets);
+		Result result = checker.check();
+		System.out.println(result);
+
+		}
+}
+
